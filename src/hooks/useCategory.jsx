b513b0b7dcbeedb05e0326097a1ed49c5693a95a
Inseo Yang <y.1nseooo@gmail.com>
2023-03-29 00:55:07 +0900

    refactor: useCategory 훅 분리, post,product 페이지 적용

---
+++ src/hooks/useCategory.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const useCategory = () => {
+  const category = (cate) => {
+    switch (cate) {
+      case 1:
+        return "여성의류";
+      case 2:
+        return "남성의류";
+      case 3:
+        return "신발";
+      case 4:
+        return "가방";
+      case 5:
+        return "시계/주얼리";
+      case 6:
+        return "패션액세서리";
+      case 7:
+        return "디지털/가전";
+      case 8:
+        return "스포츠/레저";
+      default:
+        return null;
+    }
+  };
+
+  return { category };
+};
